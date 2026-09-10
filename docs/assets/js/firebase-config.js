7340b22724fe95c8f39c97a7249ab75360c00383
José Guilherme R. <guilhermelossio8@gmail.com>
2026-09-10 11:12:38 -0300

    security: centralize Firebase config, fix 404 paths, restrict CORS

---
+++ docs/assets/js/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js - Configuração centralizada do Firebase
+// Altere a apiKey aqui ao rotacionar as credenciais.
+
+export const firebaseConfig = {
+  apiKey: "__FIREBASE_API_KEY__",
+  authDomain: "meu-app-94c74.firebaseapp.com",
+  projectId: "meu-app-94c74",
+  storageBucket: "meu-app-94c74.firebasestorage.app",
+  messagingSenderId: "711758646512",
+  appId: "1:711758646512:web:d9dae94a6d47c8f89a4635",
+  measurementId: "G-4BDC95W4VJ",
+};
